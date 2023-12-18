d68bda1bd0407e1378d7c53173f6a0594fb6c8f3
bleonjusufi <bjusufi7@gmail.com>
2023-12-18 16:21:51 +0100

    Header Completed

---
+++ frontend/src/Components/Pages/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import '../CSS/Header.css';
+import {Link} from 'react-router-dom';
+import Logo from '../Images/logo-test.png';
+
+function Header() {
+    return(
+        <div className="header">
+            <div className="container">
+            <div className="header-wrapper">
+                <div className="logo">
+                    <img src={Logo} alt="logo"></img>
+                </div>
+                <div className="navbar">
+                    <ul className="header-navigation__list">
+                        <Link to = "/" className="li">Home</Link>
+                        <Link to = "/services" className="li">Services</Link>
+                        <Link to = "/facts" className="li">Facts</Link>
+                        <Link to = "/about" className="li">About</Link>
+                        <Link to = "/pricing" className="li">Pricing</Link>
+                    </ul>
+                </div>
+                <div className="registration">
+                    <Link to = "/login" className="user-login__btn">Login</Link>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ frontend/src/Components/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+
+function Home() {
+    return(
+        <>
+        <Header />
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        </>
+    )
+}
+
+export default Home
